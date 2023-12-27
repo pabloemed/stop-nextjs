684c583255fba16c2a15db1afe515492004b123b
Pablo Medina <pmedina@litebox.ai>
2023-12-27 13:43:02 -0300

    adding a meta tag 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+      />
 
       <body className={`${inter.className} h-dvh w-dvh `}>{children}</body>
     </html>
