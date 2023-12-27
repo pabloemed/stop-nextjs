fbf6e6b6da88432ac6e395c4b338b759dd922990
Pablo Medina <pmedina@litebox.ai>
2023-12-27 13:47:29 -0300

    making changes

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,56 +1,62 @@
 "use client";
 import React, { useEffect } from "react";
 declare var window: any;
-
-function App() {
+ 
+const IndexPage = () => {
   useEffect(() => {
     const initScanner = async () => {
       try {
-        const stream = await navigator.mediaDevices.getUserMedia({
-          video: true,
-        });
-        const video: any = document.getElementById("barcode-scanner");
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        const video: any = document.getElementById('barcode-scanner');
         video.srcObject = stream;
         video.play();
 
-        window.Quagga.init(
-          {
-            inputStream: {
-              name: "Live",
-              type: "LiveStream",
-              target: video,
-            },
-            decoder: {
-              readers: ["ean_reader"],
-            },
+        window.Quagga.init({
+          inputStream: {
+            name: 'Live',
+            type: 'LiveStream',
+            target: video,
           },
-          (err: any) => {
-            if (err) {
-              console.error(err);
-              return;
-            }
-            window.Quagga.start();
+          decoder: {
+            readers: ['ean_reader'],
+          },
+        }, (err: any) => {
+          if (err) {
+            alert(err);
+            return;
           }
-        );
+          window.Quagga.onDetected(handleBarcodeDetected);
+          window.Quagga.start();
+        });
       } catch (error) {
-        console.error(error);
+        alert(error);
       }
+    };
+
+    const handleBarcodeDetected = (result: any) => {
+      const code = result.codeResult.code;
+      alert('Barcode detected: ' + code);
+      // Aquí puedes realizar acciones con el código de barras, como mostrarlo en la interfaz de usuario.
+      // Por ejemplo, podrías usar el estado de React para almacenar y mostrar el código de barras:
+      // setBarcode(code);
     };
 
     initScanner();
 
     // Cleanup function
     return () => {
-      window.Quagga?.stop();
+      window.Quagga.stop();
     };
   }, []);
 
   return (
-    <div className="App">
+    <div>
       <h1>Barcode Scanner</h1>
       <video id="barcode-scanner" width="100%" height="100%" playsInline />
+      {/* Puedes mostrar los datos del código de barras en la interfaz de usuario si es necesario. */}
+      {/* <p>Último código de barras escaneado: {barcode}</p> */}
     </div>
   );
-}
+};
 
-export default App;
+export default IndexPage;
