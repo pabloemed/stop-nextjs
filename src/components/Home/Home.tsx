e7bfe6559a97c356668b6becf8e7545dec8351ba
Pablo Medina <pmedina@litebox.ai>
2023-12-27 13:59:05 -0300

    making changes 3

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,36 +1,31 @@
 "use client";
 import React, { useEffect } from "react";
+
+import Quagga from "quagga";
+
 declare var window: any;
-const Quagga = require("quagga").default; // Common JS (important: default)
 
 const IndexPage = () => {
   useEffect(() => {
     const initScanner = async () => {
       try {
-        const stream = await navigator.mediaDevices.getUserMedia({
-          video: true,
-        });
-        const video: any = document.getElementById("barcode-scanner");
-        video.srcObject = stream;
-        video.play();
-
         Quagga.init(
           {
             inputStream: {
               name: "Live",
               type: "LiveStream",
-              target: video,
+              target: document.querySelector("#barcode-scanner"),
             },
             decoder: {
-              readers: ["ean_reader"],
+              readers: ["code_128_reader"],
             },
           },
-          (err: any) => {
+          function (err: any) {
             if (err) {
-              alert(err);
+              console.log(err);
               return;
             }
-            Quagga.onDetected(handleBarcodeDetected);
+            console.log("Initialization finished. Ready to start");
             Quagga.start();
           }
         );
@@ -39,28 +34,16 @@ const IndexPage = () => {
       }
     };
 
-    const handleBarcodeDetected = (result: any) => {
-      const code = result.codeResult.code;
-      alert("Barcode detected: " + code);
-      // Aquí puedes realizar acciones con el código de barras, como mostrarlo en la interfaz de usuario.
-      // Por ejemplo, podrías usar el estado de React para almacenar y mostrar el código de barras:
-      // setBarcode(code);
-    };
-
     initScanner();
 
     // Cleanup function
-    return () => {
-      window.Quagga.stop();
-    };
   }, []);
 
   return (
     <div>
       <h1>Barcode Scanner</h1>
       <video id="barcode-scanner" width="100%" height="100%" playsInline />
-      {/* Puedes mostrar los datos del código de barras en la interfaz de usuario si es necesario. */}
-      {/* <p>Último código de barras escaneado: {barcode}</p> */}
+      
     </div>
   );
 };
